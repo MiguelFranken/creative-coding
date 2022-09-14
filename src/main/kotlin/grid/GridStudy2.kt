51c544a4d67820f0ca0977ec7c62b8c76b0c84d2
miguelfranken <miguel.franken@rwth-aachen.de>
2022-09-14 18:26:59 +0200

    Add GridStudy2

---
+++ src/main/kotlin/grid/GridStudy2.kt
@@ -0,0 +1,46 @@
+package grid
+
+import org.openrndr.WindowMultisample
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+import org.openrndr.extra.noise.Random
+import org.openrndr.extra.olive.oliveProgram
+import org.openrndr.extra.shapes.grid
+import org.openrndr.shape.Rectangle
+import org.openrndr.shape.Segment
+
+// Based on `Untitled 3` by Reiner Schneeberger
+// Based on https://github.com/hamoid/RemakeSession
+fun main() = application {
+    configure {
+        width = 640
+        height = 640
+        title = "Grid Study 2"
+        multisample = WindowMultisample.SampleCount(8)
+    }
+
+    oliveProgram {
+        val grid = drawer.bounds.grid(10, 10, 50.0, 50.0).flatten()
+
+        fun getSegments(grid: List<Rectangle>): List<Segment> {
+            return grid.map {
+                Random.pick(it.contour.segments, count = Random.int0(2))
+            }.flatten()
+        }
+
+        var lines = getSegments(grid)
+
+        mouse.buttonDown.listen {
+            Random.randomizeSeed()
+            lines = getSegments(grid)
+        }
+
+        extend {
+            drawer.clear(ColorRGBa.WHITE)
+            lines.forEach {
+                drawer.segment(it)
+            }
+        }
+    }
+}
+
