e38cd10df0579a95dcaac476792739627c0023a7
miguelfranken <miguel.franken@rwth-aachen.de>
2022-09-19 18:44:07 +0200

    Add distribution configuration classes

--- src/main/kotlin/points/PointStudy1.kt
+++ src/main/kotlin/points/PointStudy1.kt
@@ -7,49 +7,73 @@ import org.openrndr.color.rgb
 import org.openrndr.extra.gui.GUI
 import org.openrndr.extra.gui.addTo
 import org.openrndr.extra.noise.scatter
-import org.openrndr.extra.parameters.Description
-import org.openrndr.extra.parameters.DoubleParameter
-import org.openrndr.extra.parameters.OptionParameter
+import org.openrndr.extra.parameters.*
 import org.openrndr.math.Vector2
 import org.openrndr.shape.Rectangle
 import org.openrndr.shape.ShapeProvider
 
+interface PointSetConfiguration {
+    fun ShapeProvider.generatePoints(): List<Vector2>
+}
+
 enum class Distribution {
-    POISSON_1,
-    POISSON_2,
-    POISSON_WITH_OBSTACLE,
-    PHYLLOTAXIS_1,
-    PHYLLOTAXIS_2,
+    POISSON,
+    PHYLLOTAXIS,
 }
 
 fun main() = application {
     program {
+        val gui = GUI()
+
         val settings = @Description("General") object {
-            @DoubleParameter("Circle Radius", 0.1, 4.0)
+            @DoubleParameter("Circle Radius", 2.0, 4.0)
             var radius = 1.0
         }
 
+        @Description("Poisson Point Set")
+        data class PoissonPointSetConfiguration(
+            @DoubleParameter("Placement Radius", 0.1, 10.0)
+            var placementRadius: Double = 5.0
+        ): PointSetConfiguration {
+            override fun ShapeProvider.generatePoints() = scatter(placementRadius)
+        }
+
+        @Description("Phyllotaxis Point Set")
+        data class PhyllotaxisPointSetConfiguration(
+            @IntParameter("Number Points", 100, 4000)
+            var n: Int = 1000
+        ): PointSetConfiguration {
+            override fun ShapeProvider.generatePoints() = phyllotaxis(n)
+        }
+
         @Description("Point Set")
-        class PointSet(val shapeProvider: ShapeProvider) {
-            private val sets = mutableMapOf<Distribution, List<Vector2>>()
+        class PointSetCollection(val shapeProvider: ShapeProvider) {
+            private val sets = mutableMapOf<Int, List<Vector2>>()
 
             private val obstacles = listOf(
                 Pair(20.0, listOf(Vector2(-40.0, -40.0), Vector2(40.0, -40.0))),
                 Pair(40.0, listOf(Vector2(-60.0, 80.0), Vector2(60.0, 80.0)))
             )
 
+            var configurations: Map<Distribution, PointSetConfiguration> = mapOf(
+                Pair(Distribution.POISSON, PoissonPointSetConfiguration()),
+                Pair(Distribution.PHYLLOTAXIS, PhyllotaxisPointSetConfiguration())
+            ).also {
+                it.values.forEach { config ->
+                    println("Add config $config")
+                    config.addTo(gui)
+                }
+            }
+
             @OptionParameter("Distribution")
-            var distribution = Distribution.PHYLLOTAXIS_1
+            var activeDistribution = Distribution.POISSON
+
+            val activeConfiguration get() = configurations[activeDistribution]!!
 
             val points: List<Vector2>
-                get() = sets.getOrPut(distribution) {
-                    println("Calculate points for distribution $distribution")
-                    when(distribution) {
-                        Distribution.POISSON_1 -> shapeProvider.scatter(5.0)
-                        Distribution.POISSON_2 -> shapeProvider.scatter(10.0)
-                        Distribution.POISSON_WITH_OBSTACLE -> shapeProvider.scatter(3.0, obstacles = obstacles)
-                        Distribution.PHYLLOTAXIS_1 -> shapeProvider.phyllotaxis(1000)
-                        Distribution.PHYLLOTAXIS_2 -> shapeProvider.phyllotaxis(2000)
+                get() = sets.getOrPut(activeConfiguration.hashCode()) {
+                    with(activeConfiguration) {
+                        shapeProvider.generatePoints()
                     }
                 }
         }
@@ -57,11 +81,11 @@ fun main() = application {
         val rectWidth = 300.0
         val rectHeight = 300.0
         val rect = Rectangle(-rectWidth / 2.0, -rectHeight / 2.0, rectWidth, rectHeight)
-        val pointSet = PointSet(rect)
+        val pointSetCollection = PointSetCollection(rect)
 
-        extend(GUI()) {
+        extend(gui) {
             add(settings)
-            add(pointSet)
+            add(pointSetCollection)
         }
 
         extend {
@@ -74,7 +98,7 @@ fun main() = application {
 
             drawer.fill = ColorRGBa.BLACK
             drawer.stroke = null
-            drawer.circles(pointSet.points, settings.radius)
+            drawer.circles(pointSetCollection.points, settings.radius)
         }
     }
 }
