c4c45a616ac67349675b66cc9e509a022858f9e5
miguelfranken <miguel.franken@rwth-aachen.de>
2022-09-21 14:34:46 +0200

    Randomize segment stroke weight

--- src/main/kotlin/triangulation/TriangulationStudy3.kt
+++ src/main/kotlin/triangulation/TriangulationStudy3.kt
@@ -4,6 +4,7 @@ import graphMatchedSegments
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.isolated
+import org.openrndr.extra.noise.Random
 import org.openrndr.extra.noise.scatter
 import org.openrndr.math.Vector2
 import org.openrndr.shape.LineSegment
@@ -12,7 +13,7 @@ fun main() = application {
     program {
         val rect = drawer.bounds.scaledBy(0.33, 0.5, 0.5, 0.5)
 
-        val offset = 8.0
+        val offset = 12.0
         val scaledRect = rect.offsetEdges(offset)
 
         fun generateSegments(): List<LineSegment> {
@@ -50,13 +51,21 @@ fun main() = application {
             drawer.stroke = ColorRGBa.BLACK
             drawer.fill = null
 
-            drawer.isolated {
-                drawer.translate(-rect.width/2.0, -rect.height/2.0)
-                drawer.translate(-drawer.bounds.width/4.0, 0.0)
-//                drawer.rectangle(Vector2.ZERO, rect.width, rect.height)
-                drawer.lineSegments(segmentsLeft)
+            // draw segments left rect
+            Random.isolated {
+                drawer.isolated {
+                    drawer.translate(-rect.width/2.0, -rect.height/2.0)
+                    drawer.translate(-drawer.bounds.width/4.0, 0.0)
+
+                    segmentsLeft.forEach {
+                        drawer.strokeWeight = double(0.01, 5.0)
+                        drawer.lineSegment(it)
+                    }
+                }
             }
 
+
+            // draw border left rect
             drawer.isolated {
                 drawer.strokeWeight = 3.0
                 drawer.translate(-scaledRect.width/2.0, -scaledRect.height/2.0)
@@ -64,13 +73,20 @@ fun main() = application {
                 drawer.rectangle(Vector2.ZERO, scaledRect.width, scaledRect.height)
             }
 
-            drawer.isolated {
-                drawer.translate(-rect.width/2.0, -rect.height/2.0)
-                drawer.translate(drawer.bounds.width/4.0, 0.0)
-//                drawer.rectangle(Vector2.ZERO, rect.width, rect.height)
-                drawer.lineSegments(segmentsRight)
+            Random.isolated {
+                // draw segments right rect
+                drawer.isolated {
+                    drawer.translate(-rect.width/2.0, -rect.height/2.0)
+                    drawer.translate(drawer.bounds.width/4.0, 0.0)
+
+                    segmentsRight.forEach {
+                        drawer.strokeWeight = double(0.01, 5.0)
+                        drawer.lineSegment(it)
+                    }
+                }
             }
 
+            // draw border right rect
             drawer.isolated {
                 drawer.strokeWeight = 3.0
                 drawer.translate(-scaledRect.width/2.0, -scaledRect.height/2.0)
