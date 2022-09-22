b8fe373b1e60745ceded299217698cbbe19f6d89
miguelfranken <miguel.franken@rwth-aachen.de>
2022-09-22 10:51:52 +0200

    Add Triangulation Study 4

---
+++ src/main/kotlin/triangulation/TriangulationStudy4.kt
@@ -0,0 +1,61 @@
+package triangulation
+
+import graphMatchedSegments
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+import org.openrndr.color.rgb
+import org.openrndr.draw.isolated
+import org.openrndr.extra.noise.scatter
+import org.openrndr.extra.triangulation.Delaunay
+import org.openrndr.math.Vector2
+import org.openrndr.shape.Circle
+import org.openrndr.shape.Triangle
+
+fun main() = application {
+    program {
+        val circle = Circle(Vector2.ZERO, 150.0)
+
+        var debug = false
+
+        var points: List<Vector2> = listOf()
+        var triangles: List<Triangle> = listOf()
+        val segments = tryUntil(100) {
+//            points = circle.contour.equidistantPositions(60) + circle.scatter(3.0, distanceToEdge = 6.0)
+//            points = circle.contour.equidistantPositions(80) + circle.scatter(1.5, distanceToEdge = 3.0)
+            points = circle.contour.equidistantPositions(180) + circle.scatter(1.0, distanceToEdge = 1.5)
+            triangles = Delaunay.from(points).triangles()
+            graphMatchedSegments(points)
+        } ?: listOf()
+
+        mouse.buttonUp.listen {
+            debug = !debug
+        }
+
+        extend {
+            drawer.clear(ColorRGBa.WHITE)
+            drawer.translate(drawer.bounds.center)
+
+            if (debug) {
+                drawer.fill = null
+                drawer.stroke = ColorRGBa.BLACK
+                drawer.circle(circle)
+
+                drawer.fill = null
+                drawer.stroke = rgb(0.8)
+                drawer.shapes(triangles.map { it.shape })
+            }
+
+            drawer.isolated {
+                drawer.stroke = ColorRGBa.BLACK
+                drawer.strokeWeight = .4
+                drawer.lineSegments(segments)
+            }
+
+            if (debug) {
+                drawer.fill = ColorRGBa.RED
+                drawer.stroke = null
+                drawer.circles(points, 2.0)
+            }
+        }
+    }
+}
